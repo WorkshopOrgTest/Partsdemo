1b359dbfb3eea6c1c2dd8fb0bac64cb1435a79b3
Vaishnav Nugala <vaishnav.nugala@ecanarys.com>
2021-09-07 19:51:41 +0530

    Update apitest.sh

--- apitest.sh
+++ apitest.sh
@@ -4,7 +4,5 @@
 
 RESPONSE=`curl -s --request GET -H "Content-Type:application/json" -H "Authorization:Bearer $NewSecret_TOKEN" https://api.github.com/repos/CanarysPlayground/PartsUnlimited --data "${PAYLOAD}"`
 
-
-
-exit 1
+echo $RESPONSE
 
