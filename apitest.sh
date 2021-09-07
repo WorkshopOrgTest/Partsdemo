d47bf14555933ec845c42e0f6d818932d1839e33
Vaishnav Nugala <vaishnav.nugala@ecanarys.com>
2021-09-07 19:51:41 +0530

    Update apitest.sh

--- apitest.sh
+++ apitest.sh
@@ -4,7 +4,5 @@
 
 RESPONSE=`curl -s --request GET -H "Content-Type:application/json" -H "Authorization:Bearer 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" https://api.github.com/repos/CanarysPlayground/PartsUnlimited --data "${PAYLOAD}"`
 
-
-
-exit 1
+echo $RESPONSE
 
